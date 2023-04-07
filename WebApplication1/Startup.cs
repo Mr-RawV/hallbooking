514604a5e0123e3cd630fa64e553c525592ef542
Mr-RawV <ravikarkey@gmail.com>
2023-04-07 11:59:03 +0545

    edited

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Service.Hall;
@@ -25,6 +26,8 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DbContext>(options =>
+options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddSingleton<IHallService, HallService>();
             services.Configure<CookiePolicyOptions>(options =>
             {
