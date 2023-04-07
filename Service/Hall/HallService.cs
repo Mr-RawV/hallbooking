3b4818c59590d14388f2cbf9467b6cc6166c786f
Mr-RawV <ravikarkey@gmail.com>
2023-04-07 12:17:28 +0545

    db

--- Service/Hall/HallService.cs
+++ Service/Hall/HallService.cs
@@ -1,6 +1,9 @@
-﻿using Models;
+﻿using Microsoft.Extensions.Configuration;
+using Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace Service.Hall
@@ -9,7 +12,7 @@ namespace Service.Hall
     {
         public List<MeetingHallModel> GetName()
         {
-            List<MeetingHallModel> hall = new List<MeetingHallModel>();
+            /*List<MeetingHallModel> hall = new List<MeetingHallModel>();
             MeetingHallModel room = new MeetingHallModel
             {
                 RowId = 1,
@@ -24,6 +27,33 @@ namespace Service.Hall
                 IsActive = true
             };
             hall.Add(room1);
+            return hall;*/
+
+
+
+            List<MeetingHallModel> hall = new List<MeetingHallModel>();
+
+            string connString = ConfigurationExtensions.GetConnectionString(configuration, "DBConnection");
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM Movie", conn);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            DataTable dt = ds.Tables[0];
+            sda.Dispose();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                MeetingHallModel m = new MeetingHallModel
+                {
+                    RowId = (int)dr["RowId"],
+                    HallName = dr["HallName"].ToString(),
+                    IsActive= (bool)dr["IsActive"]
+                };
+                hall.Add(m);
+            }
+            conn.Close();
             return hall;
         }
     }
